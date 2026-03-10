2425c031b0ec64fe6ff81cd9bb4c1b2aee64cf5b
Isaac Longoria <isaac.longoria9136@gmail.com>
2026-03-10 15:04:12 -0500

    updated consent type

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,17 +1,7 @@
 // Create a file: types/global.d.ts or types/gtag.d.ts
 declare global {
   interface Window {
-    gtag: (
-      command: 'config' | 'event' | 'js' | 'set',
-      targetId: string | Date,
-      config?: {
-        page_path?: string;
-        page_title?: string;
-        page_location?: string;
-        [key: string]: any;
-      }
-    ) => void;
-    dataLayer: any[];
+    gtag: (command: string, id: string, params?: Record<string, any>) => void;
   }
 }
 
